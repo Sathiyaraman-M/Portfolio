a62cbd570cd524da050f5a8474cbd466f66e04ff
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-01-31 06:16:30 +0530

    Updated Masthead

--- components/sections/masthead.tsx
+++ components/sections/masthead.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Masthead = () => (
     <header className="masthead bg-primary text-white text-center">
         <div className="container d-flex align-items-center flex-column">
-            <img className="masthead-avatar mb-5" src="/avataaars.svg" alt="..."/>
+            <div className={"mb-5"} style={{borderRadius: '2.5rem', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px'}}>
+                <img className="masthead-avatar" style={{border:'0.25rem solid white', borderRadius:'2.5rem', width: '24rem'}} src="/Me.png" alt="..."/>
+            </div>
             <h1 className="masthead-heading text-uppercase mb-0">Sathiyaraman M</h1>
             <div className="divider-custom divider-light">
                 <div className="divider-custom-line"></div>
